9451026e5d1de292cc28e362609fa995edf96ef2
Kirill Anosov <69241093+KirillAn@users.noreply.github.com>
2024-03-24 13:05:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='text-extractor',
+    name='text-extra',
     version='0.1.0',
     author='KirillAn',
     author_email='kirillius.no@gmail.com',
